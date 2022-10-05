65fcb6a4b904ccd9892c3b20b343ec5f91b17345
tomolatoon <tomo2000tomo170@gmail.com>
2022-10-05 18:30:43 +0900

    fix: some styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import 'styles/globals.scss'
-import "github-markdown-css/github-markdown-light.css"
+import "github-markdown-css/github-markdown.css"
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
--- pages/web/v1/index.tsx
+++ pages/web/v1/index.tsx
@@ -2,8 +2,6 @@ import Main from 'components/Layouts/Main'
 import Markdown from 'components/modules/Markdown'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../../../styles/Home.module.scss'
 
 const Page: NextPage = () => {
   return (
