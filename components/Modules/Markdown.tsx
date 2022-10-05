ec862182afc8e8cef7a735f4bc12b3167344c7ea
tomolatoon <tomo2000tomo170@gmail.com>
2022-10-06 00:26:25 +0900

    feat: atuo-deploy test

--- components/Modules/Markdown.tsx
+++ components/Modules/Markdown.tsx
@@ -8,6 +8,7 @@ import remarkBreaks from "remark-breaks";
 import remarkDirective from "remark-directive";
 import remarkGfm from "remark-gfm";
 import remarkToc from "remark-toc";
+const remarkRemoveComments = require("remark-remove-comments");
 
 export interface Props {
     children: string;
@@ -17,6 +18,7 @@ const Markdown = ({ children }: Props) => {
     return (<ReactMarkdown 
         className={"markdown-body " + styles.markdown}
         remarkPlugins={[
+            remarkRemoveComments,
             remarkBreaks,
             remarkGfm,
             [remarkToc, { heading: "目次", ordered: true }],
